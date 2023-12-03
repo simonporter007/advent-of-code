5aefeaed14f7195d8b7619e67aff3b2ac58d6577
Simon Porter <sporter1@godaddy.com>
2023-12-03 19:33:59 +0000

    2015 day 4

---
+++ src/2015/4/problem_1/solution.ts
@@ -0,0 +1,14 @@
+import { Md5 } from 'ts-md5';
+import { getFileContents } from '@/utils/getFileContents';
+
+const file = getFileContents();
+let line: Buffer | false;
+
+while ((line = file.next())) {
+  let md5Hash = '';
+  let i = 0;
+  while (!md5Hash.startsWith('00000')) {
+    md5Hash = Md5.hashStr(`${line}${++i}`);
+  }
+  console.log(i);
+}
